8cc46955705bc72d605ef756361308b0b4cb0356
ITO-Taro <84404661+ITO-Taro@users.noreply.github.com>
2022-02-02 16:38:17 -0500

    added us housing folder and pylint to the directory

---
+++ us_housing/us_housing.py
@@ -0,0 +1,42 @@
+"""
+Exercise: download USA_Housing.csv and perform the following tasks
+1. Create a new column 'zip_code' use the zipcodes found in values under the column address
+2. Calculate the sum of all 'avg. area income' for every state
+"""
+
+import pandas as pd
+
+PATH = "/Users/taro/Desktop/Code_Immersives/PY131/us_housing"
+with open(PATH+'/us_housing.csv', 'r', encoding="utf8") as file:
+    line = file.readline()
+    line = line.replace(".", "").replace(" ", "_").lower()
+    cols = line.strip("\n").split(",")
+
+df = pd.read_csv(
+    PATH+'/us_housing.csv',
+    index_col=False,
+    names=cols
+    )
+df = df.loc[1:, :]
+
+for col in df.columns:
+    if col == 'address':
+        df[col] = df[col].apply(lambda x: x.replace("\n", " "))
+    elif col == "area_population":
+        df[col] = df[col].apply(lambda x: round(float(x)))
+    else:
+        df[col] = df[col].apply(lambda x: round(float(x), 2))
+
+for num in df.index:
+    addresses = df.loc[num, 'address'].split(" ")
+    df.loc[num, 'state'] = addresses[-2]
+    df.loc[num, 'zip_code'] = addresses[-1]
+
+mean_income_per_state = {}
+states = df.state.unique()
+states.sort()
+for state in states:
+    mean_income_per_state[state] = df[df.state == state].avg_area_income.sum().round(2)
+
+if __name__ == '__main__':
+    print(sorted(mean_income_per_state.items(), key=lambda kv: kv[1], reverse=True))
